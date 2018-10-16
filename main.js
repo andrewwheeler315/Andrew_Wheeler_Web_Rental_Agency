3da7c0040c3f6d1a5620797381b2af626deb4590
andrewwheeler315 <awheeler@basecampcodingacademy.org>
2018-10-16 10:36:23 -0500

    I've added var elements in the js file and put id names on all the h3 tag in my html.

---
+++ main.js
@@ -0,0 +1,57 @@
+var movieTitle = [
+    "Deadpool",
+    "Iron Man",
+    "Thor",
+    "Captain America: The First Avenger",
+    "The Avengers",
+    "Deadpool 2",
+    "Iron Man 3",
+    "Thor Ragnarok",
+    "Captain America: Civil War",
+    "The Avengers: Infinity War"
+];
+document.getElementById("movie_title").innerHTML = movieTitle;
+
+var bookTitle= [
+    "39 Clues: The Maze of Bones",
+    "The Maze Runner",
+    "The Fellowship of the Ring",
+    "Harry Potter and the Sorcerer's Stone",
+    "Ranger's Apprentice: The Ruins of Gorlan",
+    "39 Clues: Into the Gauntlet",
+    "The Death Cure", 
+    "The Return of the King",
+    "Harry Potter and the Deathly Hollows",
+    "Ranger's Apprentice: The Royal Ranger"
+];
+document.getElementById("book_title").innerHTML = bookTitle;
+
+var mangaTitle = [
+    "Dragon Ball Vol. 1",
+    "Bleach Vol. 1",
+    "Naruto Vol. 1",
+    "Fairy Tail Vol. 1",
+    "One Piece Vol. 1",
+    "Dragon Ball Super Vol. 7",
+    "Bleach Vol. 74",
+    "Naruto Vol. 72",
+    "Fairy Tail Vol. 63",
+    "One Piece Vol. 84"
+];
+document.getElementById("manga_title").innerHTML = mangaTitle;
+
+
+var gameTitle = [
+    "Final Fantasy",
+    "Gears of War",
+    "God of War",
+    "Uncharted: Drake's Fortune",
+    "Resident Evil I",
+    "Final Fantasy XV",
+    "Gears of War 3",
+    "God of War 4",
+    "Uncharted 4: A Thief's End",
+    "Resident Evil VII: Biohazard"
+];
+document.getElementById("game_title").innerHTML = gameTitle;
+
